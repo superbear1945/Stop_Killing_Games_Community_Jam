11ed0aef70d3dd09471caa4a713260393fa681cc
rain-zimujun <1738388256@qq.com>
2025-07-14 18:58:08 +0800

    Update Shoot.cs

--- Assets/Script/Player/Shoot.cs
+++ Assets/Script/Player/Shoot.cs
@@ -2,17 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Shoot : MonoBehaviour
 {
+    // Start is called before the first frame update
     [Header("甩杆参数")]
-    public float castForce = 10f;      // 甩杆力度
-    public float shootDelay = 0.5f;//甩杆动作延迟（s）
-    public GameObject yuEr;//鱼饵预制体
+    public float _castForce = 10f;      // 甩杆力度
+    public float _shootDelay = 0.5f;//甩杆动作延迟（s）
+    public GameObject _fishingBait;//鱼饵预制体
     private bool isShooting = false;//是否开始钓鱼了
 
     void Update()
     {
+        Console.WriteLine();
+        
         //检测右键是否按下
         if (Input.GetMouseButtonDown(1) && !isShooting)
         {
@@ -20,6 +24,8 @@ public class Shoot : MonoBehaviour
         }
     }
 
+    // Update is called once per frame
+    void Update()
     void StartShoot()
     {
         isShooting = true;
@@ -27,21 +33,22 @@ public class Shoot : MonoBehaviour
         //甩杆动作
         //GetComponent<Animator>().SetTrigger("Cast");
         //生成鱼饵
-        Invoke("SpawnBait", shootDelay);
+        Invoke("SpawnBait", _shootDelay);
     }
     void SpawnBait()
     {
-        if (yuEr != null)
+        
+        if (_fishingBait != null)
         {
             //生成鱼饵
             Vector2 spawnPos = (Vector2)transform.position + Vector2.right * 1f;//向右甩杆
             // 实例化鱼饵
-            GameObject bait = Instantiate(yuEr, spawnPos, Quaternion.identity);
+            GameObject bait = Instantiate(_fishingBait, spawnPos, Quaternion.identity);
              // 添加 2D 物理力
             Rigidbody2D baitRb = bait.GetComponent<Rigidbody2D>();
             if (baitRb != null)
             {
-                baitRb.AddForce(Vector2.right * castForce, ForceMode2D.Impulse); // 向右施力
+                baitRb.AddForce(Vector2.right * _castForce, ForceMode2D.Impulse); // 向右施力
             }
         }
         isShooting = false;
