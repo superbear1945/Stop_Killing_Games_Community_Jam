d62676d0f8a3ac8f75db2053e3cbe9c3df3160e3
superbear1945 <2689505220@qq.com>
2025-07-15 01:13:05 +0800

    完成玩家左右转身的逻辑

---
+++ Assets/Script/Player/FlipSprite.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipSprite : MonoBehaviour
+{
+    Rigidbody2D _rb2d;
+
+    void Awake()
+    {
+        // 获取 Rigidbody2D 组件
+        _rb2d = GetComponent<Rigidbody2D>();
+        if (_rb2d == null)
+            Debug.LogError("未找到Rigidbody2D组件");
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(_rb2d.velocity.x);
+        if (_rb2d.velocity.x > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (_rb2d.velocity.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+    }
+}
