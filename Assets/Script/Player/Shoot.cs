7b06c61adc9e8f9c6e219a9fcceec452b006fee4
rain-zimujun <1738388256@qq.com>
2025-07-14 17:39:27 +0800

    upload new unity project

---
+++ Assets/Script/Player/Shoot.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Console.WriteLine();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
